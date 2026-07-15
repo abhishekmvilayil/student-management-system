963a6b2b66658849c1f65cf14bef7afb8ee936d0
Abhishek <abhishekvilayil628@gmail.com>
2026-07-15 13:19:45 +0530

    Costructor in Student class  and Main class created

--- Student.java
+++ Student.java
@@ -5,6 +5,14 @@ public class Student {
     private int age;
     private String course;
 
+    public Student(int id,String name,int age,String course){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.course=course;
+    }
+    
+    @Override
     public String toString(){
         return "ID:"+id+
                 ",Name:"+name+
