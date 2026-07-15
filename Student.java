8f22f16d4e73e857fe8c7ec8da0b3792000ca19a
Abhishek <abhishekvilayil628@gmail.com>
2026-07-15 13:12:18 +0530

    String return function added in the student class

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private String course;
+
+    public String toString(){
+        return "ID:"+id+
+                ",Name:"+name+
+                ",Age:"+age+
+                ",Course:"+course;
+    }
+}
